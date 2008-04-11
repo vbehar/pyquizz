5a07122c4dfa553cfbc22db9e75eb7502fc67d35
Vincent Behar <vinz@pc-vincent.(none)>
2008-04-11 14:33:57 +0200

    add templates for default handler (index + 404)

--- python/app/web/__init__.py
+++ python/app/web/__init__.py
@@ -1,1 +1,8 @@
 #!/usr/bin/env python
+
+import os
+from google.appengine.ext.webapp import template
+
+def render_template(requestHandler, template_file, dictionary = {}):
+    path = os.path.join(os.path.dirname(__file__), 'templates', template_file)
+    requestHandler.response.out.write(template.render(path, dictionary))
--- python/app/web/default_handler.py
+++ python/app/web/default_handler.py
@@ -2,13 +2,13 @@
 
 from google.appengine.ext import webapp
 
+from app.web import *
+
 class Home(webapp.RequestHandler):
     def get(self):
-        self.response.headers['Content-Type'] = 'text/html'
-        self.response.out.write('homepage')
+        render_template(self, 'index.html')
 
 class NotFound(webapp.RequestHandler):
     def get(self, page_name):
-        self.response.headers['Content-Type'] = 'text/html'
-        self.response.out.write('404 on page :  %s' % (page_name))
+        render_template(self, '404.html', {'page_name':page_name})
 
